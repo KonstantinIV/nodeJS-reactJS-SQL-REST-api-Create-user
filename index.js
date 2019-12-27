36154e4496f55d02a8f73dacced94674ab38b062
root <root@debian.deb>
2019-12-27 18:59:44 +0000

    add css html

--- index.js
+++ index.js
@@ -5,18 +5,23 @@ var  fs = require('fs');
 const hostname = '192.168.1.246';
 const port = 3001;
 
- 
 
 
-fs.readFile('./index.html', function (err, html) {
-  if (err) {
-    throw err; 
-  } 
-  const server = http.createServer(function(request, response) { 
-    response.writeHeader(200, {"Content-Type": "text/html"}); 
+
+const server = http.createServer(function(request, response) { 
+  response.writeHeader(200, {"Content-Type": "text/html"}); 
+  
+  fs.readFile('./index.html', function (err, html) {
+    if (err) {
+      throw err; 
+    } 
     response.write(html); 
     response.end(); 
-  }).listen(port, hostname, () => {
-     console.log(`Server running at http://${hostname}:${port}/`);
-     });
- });
+    
+   }); 
+  
+  
+}).listen(port, hostname, () => {
+   console.log(`Server running at http://${hostname}:${port}/`);
+   });
+
